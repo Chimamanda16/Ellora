f98d67a124643d05d8c8cc7879f2b02ad9902103
Chimamanda16 <chimamandaunachukwu@gmail.com>
2024-12-28 22:22:13 +0100

    Update heading

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,7 +8,7 @@ const AboutComp = () => {
 
             <motion.div className="about-text" initial={{opacity:0, y: 50}} whileInView={{opacity: 1, y: 0, transition: {duration: 1, delay: 0.5}}}>
                 <h3>Freshly Curated Gift Boxes</h3>
-                <h2 className="about-title">Thoughtful Surprises From Our Hands to Your Heart</h2>
+                <h2 className="about-title">Gifts From Our Hearts to Your Doorstep</h2>
                 <p>From Christmas, to Valentines, to graduations and birthdays, we got you covered!</p>
                 <button className="about-btn">SHOP NOW</button>
             </motion.div>
